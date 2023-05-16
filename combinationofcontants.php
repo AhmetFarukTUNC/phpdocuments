8a7f6c97f0c728be2f9498564e5dbe694f0669bb
YAZILIMCIABII <129683968+AhmetFarukTUNC@users.noreply.github.com>
2023-05-16 17:21:35 +0300

    combinationofcontants.php
    combinationofcontants.php

---
+++ combinationofcontants.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        // define("name","Ahmet Faruk ");
+
+        // define("surname","TUNÇ");
+        
+        // define("value",name.surname);
+
+        // echo value;
+
+        // const name = "Ahmet Faruk";
+
+        // const surname = " TUNÇ";
+
+        // const value = name . surname;
+
+        // echo value;
+
+        
+    ?>
+</body>
+</html>
