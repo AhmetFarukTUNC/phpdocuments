a0fa87062ea8a8be398cf914abe34531c43634a5
YAZILIMCIABII <129683968+AhmetFarukTUNC@users.noreply.github.com>
2023-05-18 21:55:24 +0300

    combinationofconstant2.php
    combinationofconstant2.php

---
+++ combinationofconstant2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        // define("name","Ahmet Faruk");
+
+        // echo name[0]."<br>";
+
+        // echo name[1]."<br>";
+
+        // echo name[2]."<br>";
+
+        // echo name[3]."<br>";
+
+        // echo name[4]."<br>";
+
+        // echo name[5]."<br>";
+
+        // const name = "Ahmet Faruk";
+
+        // echo name[0]."<br>";
+
+        // echo name[1]."<br>";
+
+        // echo name[2]."<br>";
+
+        // echo name[3]."<br>";
+
+        // echo name[4]."<br>";
+
+        // echo name[5]."<br>";
+
+    ?>
+</body>
+</html>
